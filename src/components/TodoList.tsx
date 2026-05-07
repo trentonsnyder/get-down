14fe02bdf3406bd59519511fc0a43fd8d135261d
Trenton <17258002+trentonsnyder@users.noreply.github.com>
2026-05-07 09:41:57 -0600

    add TodoItem and TodoList components

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,22 @@
+import { Todo } from '../types/todo'
+import { TodoItem } from './TodoItem'
+
+type Props = {
+  todos: Todo[]
+  onToggle: (id: string) => void
+  onDelete: (id: string) => void
+}
+
+export function TodoList({ todos, onToggle, onDelete }: Props) {
+  if (todos.length === 0) {
+    return <p>No todos yet.</p>
+  }
+
+  return (
+    <ul>
+      {todos.map(todo => (
+        <TodoItem key={todo.id} todo={todo} onToggle={onToggle} onDelete={onDelete} />
+      ))}
+    </ul>
+  )
+}
